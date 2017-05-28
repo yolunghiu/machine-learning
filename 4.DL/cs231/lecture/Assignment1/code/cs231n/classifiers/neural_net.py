a7df9c9cf79420e58a7af33a2a420cd9967577bd
liuhy <190978752@qq.com>
2017-05-28 18:43:22 +0800

    两层神经网络的作业算是做完了，自己调的最高就到38.3，往上的代码也差不多

--- 4.DL/cs231/lecture/Assignment1/code/cs231n/classifiers/neural_net.py
+++ 4.DL/cs231/lecture/Assignment1/code/cs231n/classifiers/neural_net.py
@@ -37,9 +37,12 @@ class TwoLayerNet(object):
         """
         self.params = {}
         self.params['W1'] = std * np.random.randn(input_size, hidden_size)
+        # self.params['W1'] = std * np.random.randn(input_size, hidden_size) / np.sqrt(2.0/(input_size))
         self.params['b1'] = np.zeros(hidden_size)
         self.params['W2'] = std * np.random.randn(hidden_size, output_size)
+        # self.params['W2'] = std * np.random.randn(hidden_size, output_size) / np.sqrt(2.0/(hidden_size))
         self.params['b2'] = np.zeros(output_size)
+
 
     def loss(self, X, y=None, reg=0.0):
         """
@@ -72,7 +75,7 @@ class TwoLayerNet(object):
         # Compute the forward pass
         scores = None
         # ReLu激活函数只在hidden layer使用
-        hidden = np.maximum(0, np.dot(X, W1) + b1)
+        hidden = np.maximum(0, np.dot(X, W1) + b1) * 1.0
         scores = np.dot(hidden, W2) + b2
         pass
 
@@ -85,9 +88,9 @@ class TwoLayerNet(object):
 
         # data loss
         # 减去最大值会使下面的计算更稳定，且对结果没有影响
-        scores_max = np.max(scores,axis=1,keepdims=True)
+        scores_max = np.max(scores,axis=1,keepdims=True) * 1.0
         exp_scores = np.exp(scores - scores_max)
-        probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+        probs = exp_scores / np.sum(exp_scores, dtype=float, axis=1, keepdims=True)
         # print(np.sum(exp_scores, axis=1, keepdims=True))
         corect_logprobs = -np.log(probs[range(N), y])
         data_loss = np.sum(corect_logprobs) / N
@@ -149,7 +152,7 @@ class TwoLayerNet(object):
         num_train = X.shape[0]
         # 这个参数的意思是所有样本完成一次正向传递和反向传递需要多少次迭代
         # 如果num_train < batch_size(toy data),这个参数设为1
-        iterations_per_epoch = max(num_train / batch_size, 1)
+        iterations_per_epoch = int(max(num_train / batch_size, 1))
 
         # Use SGD to optimize the parameters in self.model
         loss_history = []
@@ -163,13 +166,13 @@ class TwoLayerNet(object):
         # mW2 = np.zeros_like(self.params['W2'])
         # mb1 = np.zeros_like(self.params['b1'])
         # mb2 = np.zeros_like(self.params['b2'])
-        vW1 = np.zeros_like(self.params['W1'])
-        vW2 = np.zeros_like(self.params['W2'])
-        vb1 = np.zeros_like(self.params['b1'])
-        vb2 = np.zeros_like(self.params['b2'])
-
-        mu_all = [0.5,0.9,0.95,0.99]
-        mu = mu_all[0]
+        # vW1 = np.zeros_like(self.params['W1'])
+        # vW2 = np.zeros_like(self.params['W2'])
+        # vb1 = np.zeros_like(self.params['b1'])
+        # vb2 = np.zeros_like(self.params['b2'])
+        #
+        # mu_all = [0.5,0.9,0.95,0.99]
+        # mu = mu_all[0]
 
         for it in range(num_iters):
             X_batch = None
@@ -179,13 +182,9 @@ class TwoLayerNet(object):
             # TODO: Create a random minibatch of training data and labels, storing  #
             # them in X_batch and y_batch respectively.                             #
             #########################################################################
-            if iterations_per_epoch == 1:
-                X_batch = X
-                y_batch = y
-            else:
-                idx = np.random.randint(0,num_train,size=batch_size)
-                X_batch = X[idx,:]
-                y_batch = y[idx]
+            idx = np.random.choice(num_train, batch_size, replace=True)
+            X_batch = X[idx,:]
+            y_batch = y[idx]
 
             pass
 
@@ -200,20 +199,24 @@ class TwoLayerNet(object):
             # stored in the grads dictionary defined above.                         #
             #########################################################################
 
-            # 梯度更新，这里使用NN_3中介绍的一种方法：Adam
             dW1 = grads['W1']
             dW2 = grads['W2']
             db1 = grads['b1']
             db2 = grads['b2']
+            self.params['W1'] -= learning_rate*dW1
+            self.params['W2'] -= learning_rate*dW2
+            self.params['b1'] -= np.reshape(learning_rate*db1, self.params['b1'].shape)
+            self.params['b2'] -= np.reshape(learning_rate*db2, self.params['b2'].shape)
 
-            vW1 = mu * vW1 -learning_rate * dW1
-            vW2 = mu * vW2 - learning_rate * dW2
-            vb1 = mu * vb1 - learning_rate * db1
-            vb2 = mu * vb2 - learning_rate * db2
-            self.params['W1'] += vW1
-            self.params['W2'] += vW2
-            self.params['b1'] += np.reshape(vb1, self.params['b1'].shape)
-            self.params['b2'] += np.reshape(vb2, self.params['b2'].shape)
+            # 梯度更新，这里使用NN_3中介绍的一种方法：Adam
+            # vW1 = mu * vW1 -learning_rate * dW1
+            # vW2 = mu * vW2 - learning_rate * dW2
+            # vb1 = mu * vb1 - learning_rate * db1
+            # vb2 = mu * vb2 - learning_rate * db2
+            # self.params['W1'] += vW1
+            # self.params['W2'] += vW2
+            # self.params['b1'] += np.reshape(vb1, self.params['b1'].shape)
+            # self.params['b2'] += np.reshape(vb2, self.params['b2'].shape)
 
             # mW1 = beta1*mW1 + (1-beta1)*dW1
             # vW1 = beta2*vW1 + (1-beta2)*(dW1**2)
@@ -243,12 +246,13 @@ class TwoLayerNet(object):
 
                 # Decay learning rate
                 learning_rate *= learning_rate_decay
-            if it>50 and it<=100 :
-                mu = mu_all[1]
-            elif it>100 and it<=150:
-                mu = mu_all[2]
-            elif it>150:
-                mu = mu_all[3]
+
+            # if it>50 and it<=100 :
+            #     mu = mu_all[1]
+            # elif it>100 and it<=150:
+            #     mu = mu_all[2]
+            # elif it>150:
+            #     mu = mu_all[3]
 
         return {
             'loss_history': loss_history,
@@ -279,9 +283,9 @@ class TwoLayerNet(object):
         ###########################################################################
         hidden = np.dot(X,self.params['W1']) + self.params['b1']
         scores = np.dot(hidden,self.params['W2']) +self.params['b2']
-        maxScores = np.max(scores, axis=1, keepdims=True)
-        [n,c] = np.where(scores == maxScores)
-        y_pred = c
+        # maxScores = np.max(scores, axis=1, keepdims=True)
+        # [n,c] = np.where(scores == maxScores)
+        y_pred = np.argmax(scores,axis=1)
 
         pass
         ###########################################################################
